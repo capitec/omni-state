8076b882372b1a27919702dc2a0221b9eb859bcf
jn42lm1 <54233338+jn42lm1@users.noreply.github.com>
2023-07-17 13:28:14 +0200

    Update src/types/AsyncStorage.ts
    Co-authored-by: Stefan Buys <10812446+stefan505@users.noreply.github.com>

--- src/types/AsyncStorage.ts
+++ src/types/AsyncStorage.ts
@@ -8,7 +8,7 @@ export interface AsyncStorage {
 	 * 
 	 * @param key - The key under which the value is stored.
 	 * 
-	 * @returns The stored value parsed from JSON, or null if not set.
+	 * @returns The stored value parsed from JSON, or undefined if not set.
 	 */
 	get<T>(key: string): Promise<T | undefined>;
 
